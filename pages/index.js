3b6bc53e910752664dc8a82d953e9731ad44a648
BrendonAraujo <61763480+BrendonAraujo@users.noreply.github.com>
2025-05-11 12:50:30 +0000

    #ADD Elóisa

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>!A!</h1>
+    return <h1>Eloísa! O mundo sempre será mais colorido quando estou contigo! Decidi não te guardar apenas na memória, mas num pedacinho da WEB e assim, mesmo que meu cérebro se já, algum pedacinho de tudo que senti contigo continuará pela eternidade aqui!</h1>
 }
 
 export default Home;
