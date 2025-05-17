a32f9c8905248007ffdc96451dcbe4b21e971144
BrendonAraujo <61763480+BrendonAraujo@users.noreply.github.com>
2025-05-17 11:01:01 +0000

    modificou a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>Eloísa! O mundo sempre será mais colorido quando estou contigo! Decidi não te guardar apenas na memória, mas num pedacinho da WEB e assim, mesmo que meu cérebro se já, algum pedacinho de tudo que senti contigo continuará pela eternidade aqui!</h1>
+    return <h1>Eloísa! <br/>O mundo sempre será mais colorido quando estou contigo!<br/>Decidi não te guardar apenas na memória, mas num pedacinho da WEB e assim, mesmo que meu cérebro se vá, algum pedacinho de tudo que senti contigo continuará pela eternidade aqui!</h1>
 }
 
 export default Home;
+
